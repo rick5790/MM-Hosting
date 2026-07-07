41f839f5339e9689822290ee567979dadaa4896b
Rick Ou <xavier1101@outlook.com>
2026-07-07 12:04:00 -0700

    Remove lightsail-backend from frontend repo (backend belongs in Makkie-Backend)

--- lightsail-backend/makkie-web-api/routes/productRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const productController = require("../controllers/productController");
-
-router.get("/", productController.index);
-
-module.exports = router;
